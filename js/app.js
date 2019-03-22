7cde0c9a0465739cbb32cbca8e2d0f5ff3e178c6
Alex Hughes <alexhugh121@gmail.com>
2019-03-21 18:24:54 -0700

    Commit 6 - Prime Numbers.

--- js/app.js
+++ js/app.js
@@ -25,11 +25,11 @@
 
 //PYTHAGORAS//
 
-function aBC(a,b){
-    c= Math.sqrt((a**2 + b**2));
-    return c;
-}
-console.log(aBC(8,6));
+// function aBC(a,b){
+//     c= Math.sqrt((a**2 + b**2));
+//     return c;
+// }
+// console.log(aBC(8,6));
 
 //SUM ARRAY//
 // function sumArray(arr){
@@ -41,3 +41,32 @@ console.log(aBC(8,6));
 // }
 
 // console.log(sumArray([1,1,1,1,1,1,1,1,1,1]))
+
+//PRIME NUMBERS//
+
+// A Prime number is a number that is not evenly 
+// divisible by another number except 1 and itself. 
+// If you want to read more deeply about it, [go here]
+// (https://en.wikipedia.org/wiki/Prime_number).
+// To test whether a number is Prime, you only need 
+// to test as far as the **square root** of that 
+// number. This is advisable for optimization and 
+// testing large numbers.
+
+function checkPrime(x){
+    for(let i=2; i < x; i++){
+        if(x%i=== 0){
+            return false
+        }
+    }return true;
+    };
+console.log(checkPrime(29))
+function printPrimes(x){
+    for(let i=0; i <= x; i++){
+        if(checkPrime(i) === true){
+            console.log(i)
+        }
+    }
+}
+
+printPrimes(400)
