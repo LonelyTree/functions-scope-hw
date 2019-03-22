0f0e3427842444dde937fbc0bbeca421a885de3f
Alex Hughes <alexhugh121@gmail.com>
2019-03-21 17:54:36 -0700

    Commit 5 - Sum Array.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,43 @@
 //PALINDROME//
 
-function checkPalindrome(word) {
-    if(word.toLowerCase() === word.toLowerCase().split("").reverse().join(""))
-    {
-        return true
-} return false
-    }
+// function checkPalindrome(word) {
+//     if(word.toLowerCase() === word.toLowerCase().split("").reverse().join(""))
+//     {
+//         return true
+// } return false
+//     }
 
-console.log(checkPalindrome("Justin"))
+// console.log(checkPalindrome("Justin"))
 
 //DIGIT SUM//
 
-function sumDigits(x) {
-    let sum = 0;
-    let numString = x.toString().split("");
-    for(let i=0; i < numString.length; i++){
-        sum = sum + +numString[i]
-    }
-return sum;
+// function sumDigits(x) {
+//     let sum = 0;
+//     let numString = x.toString().split("");
+//     for(let i=0; i < numString.length; i++){
+//         sum = sum + +numString[i]
+//     }
+// return sum;
 
-}
+// }
 
-console.log(sumDigits(11111))
+// console.log(sumDigits(11111))
 
 //PYTHAGORAS//
 
 function aBC(a,b){
-    c=(a**2 + b**2);
-    Math.sqrt(c);
+    c= Math.sqrt((a**2 + b**2));
     return c;
 }
-aBC(1,2);
+console.log(aBC(8,6));
+
+//SUM ARRAY//
+// function sumArray(arr){
+//     sum = 0;
+// for(let i=0; i < arr.length; i++){
+//             sum = sum + +arr[i]
+//         }
+//     return sum;
+// }
+
+// console.log(sumArray([1,1,1,1,1,1,1,1,1,1]))
