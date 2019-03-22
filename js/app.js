8a9adc311ac849e974249f3533ff3d8325de91cb
Alex Hughes <alexhugh121@gmail.com>
2019-03-21 19:12:23 -0700

    insertDash

--- js/app.js
+++ js/app.js
@@ -44,29 +44,49 @@
 
 //PRIME NUMBERS//
 
-// A Prime number is a number that is not evenly 
-// divisible by another number except 1 and itself. 
-// If you want to read more deeply about it, [go here]
-// (https://en.wikipedia.org/wiki/Prime_number).
-// To test whether a number is Prime, you only need 
-// to test as far as the **square root** of that 
-// number. This is advisable for optimization and 
-// testing large numbers.
+// function checkPrime(x){
+//     for(let i=2; i < x; i++){
+//         if(x%i=== 0){
+//             return false
+//         }
+//     }return true;
+//     };
+// console.log(checkPrime(29))
+// function printPrimes(x){
+//     for(let i=0; i <= x; i++){
+//         if(checkPrime(i) === true){
+//             console.log(i)
+//         }
+//     }
+// }
 
-function checkPrime(x){
-    for(let i=2; i < x; i++){
-        if(x%i=== 0){
-            return false
-        }
-    }return true;
-    };
-console.log(checkPrime(29))
-function printPrimes(x){
-    for(let i=0; i <= x; i++){
-        if(checkPrime(i) === true){
-            console.log(i)
+// printPrimes(400)
+
+//HUNGRY FOR MORE//
+
+// 2. Write a function `insertDash` that accepts 
+// a number as a parameter and returns a string with 
+// a dash inserted between any consecutive
+//  **odd numbers**. There should not be a dash at 
+//  the end, it goes only between numbers.
+// ```javascript
+// console.log(insertDash(454793));
+
+function insertDash(num) {
+    
+    let arr = num.toString().split("")
+    const result = []
+    for(let i=0; i <= arr.length; i++){
+        if(((arr[i-1])%2 ===1)&&(arr[i]%2 ===1)){
+            result.push("-",arr[i]);
+        } else {
+            result.push(arr[i])
         }
     }
+    console.log(result.join(""))
 }
 
-printPrimes(400)
+insertDash(5555777777999993333);
+rv = 10
+let raw = rv.toString().split("")
+console.log(raw)
